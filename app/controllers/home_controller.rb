dd15bed85b13473d1cf1b51e7a456048b308c3ed
uros <stumpfl.uros@gmail.com>
2013-03-04 09:43:22 +0100

    Giving Shared Folder Access to Other Users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,35 @@
 class HomeController < ApplicationController
-
   def index
     if user_signed_in?
-      #show only root folders (which have no parent folders)
-      @folders = current_user.folders.roots
+      #show folders shared by others
+      @being_shared_folders = current_user.shared_folders_by_others
 
-      #show only root files which has no "folder_id"
+      #show only root folders
+      @folders = current_user.folders.roots
+      #show only root files
       @assets = current_user.assets.where("folder_id is NULL").order("uploaded_file_file_name desc")
     end
-
   end
-  #this action is for viewing folders
+
   def browse
-    #get the folders owned/created by the current_user
-    @current_folder = current_user.folders.find(params[:folder_id])
+    #first find the current folder within own folders
+    @current_folder = current_user.folders.find_by_id(params[:folder_id])
+    @is_this_folder_being_shared = false if @current_folder #just an instance variable to help hiding buttons on View
+
+    #if not found in own folders, find it in being_shared_folders
+    if @current_folder.nil?
+      folder = Folder.find_by_id(params[:folder_id])
+
+      @current_folder ||= folder if current_user.has_share_access?(folder)
+      @is_this_folder_being_shared = true if @current_folder #just an instance variable to help hiding buttons on View
+
+    end
 
     if @current_folder
+      #if under a sub folder, we shouldn't see shared folders
+      @being_shared_folders = []
 
-      #getting the folders which are inside this @current_folder
+      #show folders under this current folder
       @folders = @current_folder.children
 
       #show only files under this current folder
@@ -25,11 +37,12 @@ class HomeController < ApplicationController
 
       render :action => "index"
     else
-      flash[:error] = "Don't be cheeky! Mind your own folders!"
+      flash[:error] = "Don't be cheeky! Mind your own assets!"
       redirect_to root_url
     end
   end
-  #this handles ajax request for inviting others to share folders
+
+
   def share
     #first, we need to separate the emails with the comma
     email_addresses = params[:email_addresses].split(",")
@@ -49,6 +62,7 @@ class HomeController < ApplicationController
       @shared_folder.save
 
       #now we need to send email to the Shared User
+      UserMailer.invitation_to_share(@shared_folder).deliver
     end
 
     #since this action is mainly for ajax (javascript request), we'll respond with js file back (refer to share.js.erb)
@@ -57,14 +71,4 @@ class HomeController < ApplicationController
       }
     end
   end
-
-  #now send email to the recipients
-  UserMailer.invitation_to_share(@shared_folder).deliver
-
-
-
 end
-
-
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,40 +1,25 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :name, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  # attr_accessible :title, :body
 
   validates :email, :presence => true, :uniqueness => true
 
   has_many :assets
-
-
-
-  attr_accessible :user_id, :uploaded_file
-
-
-  belongs_to :user
-
-#set up "uploaded_file" field as attached_file (using Paperclip)
-  has_attached_file :uploaded_file
-
-  validates_attachment_size :uploaded_file, :less_than => 10.megabytes
-  validates_attachment_presence :uploaded_file
-
   has_many :folders
-
-  #this is for folders which this user has shared
   has_many :shared_folders, :dependent => :destroy
-
-  #this is for folders which the user has been shared by other users
   has_many :being_shared_folders, :class_name => "SharedFolder", :foreign_key => "shared_user_id", :dependent => :destroy
+  has_many :shared_folders_by_others, :through => :being_shared_folders, :source => :folder
+
 
   after_create :check_and_assign_shared_ids_to_shared_folders
 
-  #this is to make sure the new user ,of which the email addresses already used to share folders by others, to have access to those folders
   def check_and_assign_shared_ids_to_shared_folders
     #First checking if the new user's email exists in any of ShareFolder records
     shared_folders_with_same_email = SharedFolder.find_all_by_shared_email(self.email)
@@ -47,4 +32,32 @@ class User < ActiveRecord::Base
       end
     end
   end
+
+  # to check if a user has access to this specific folder
+  def has_share_access?(folder)
+    # doesn't have share access if folder is nil (i.e., file is top-level)
+    return false if folder.nil?
+
+    # has share access if the folder is one of his own
+    return true if self.folders.include?(folder)
+
+    # has share access if the folder is one of the shared_folders_by_others
+    return true if self.shared_folders_by_others.include?(folder)
+
+    # for checking sub folders under one of the being_shared_folders
+    return_value = false
+
+    folder.ancestors.each do |ancestor_folder|
+      return_value = self.being_shared_folders.include?(ancestor_folder)
+      if return_value # if its true
+        return true
+      end
+    end
+
+    return false
+  end
+
+
+
+
 end
